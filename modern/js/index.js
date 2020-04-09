93954cd2810f6442be4eab64c88c9028080dfcff
myunghwan <0x4861@neotrilabs.com>
2020-04-09 18:15:59 +0900

    [Add] function

--- modern/js/index.js
+++ modern/js/index.js
@@ -1,13 +1,18 @@
-// 조건문
-const device='iphone';
-switch(device) {
-    case 'iphone':
-        console.log('아이폰');
-        break;
-    case 'ipad':
-        console.log('ipad');
-        break;
-    default:
-        console.log('unknown..');
+// 함수
+function add(a, b) {
+    return a+b;
 }
 
+function hello(name) {
+    console.log('Hello, ' + name + '!');
+}
+
+function hello2(name) {
+    console.log(`Hello ${name}!`);
+    return `Hello ${name}!`;
+}
+
+const sum = add(1,2);
+console.log(sum);
+console.log(hello('tom'));
+console.log(hello2('tom2'));
