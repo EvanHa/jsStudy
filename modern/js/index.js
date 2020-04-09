bcbb15d50b7f7fa783ea16565f5816cedecefc7d
myunghwan <0x4861@neotrilabs.com>
2020-04-09 18:20:21 +0900

    [Add] arrow function

--- modern/js/index.js
+++ modern/js/index.js
@@ -1,18 +1,8 @@
-// 함수
-function add(a, b) {
+// arrow function (es6 function)
+
+const add = (a,b) => {
     return a+b;
-}
-
-function hello(name) {
-    console.log('Hello, ' + name + '!');
-}
-
-function hello2(name) {
-    console.log(`Hello ${name}!`);
-    return `Hello ${name}!`;
 }
 
 const sum = add(1,2);
 console.log(sum);
-console.log(hello('tom'));
-console.log(hello2('tom2'));
