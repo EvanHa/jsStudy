6ca5fd1f2f6fc8f3c9608707a95c6f5a304c0402
myunghwan <0x4861@neotrilabs.com>
2020-04-09 16:07:21 +0900

    [Add] 논리연산자

--- modern/js/index.js
+++ modern/js/index.js
@@ -1,5 +1,17 @@
-let a = 1;
-alert(a++); // 1
-alert(a);   // 2
-alert(++a); // 3
+// 논리연산자
+// 1. NOT !
+// const a = !true;
+// alert(a);
+
+// 2. AND &&
+// const a = true && true;
+// alert(a);
+// const b = true && false;
+// alert(b);
+
+// 3. OR  ||
+const a = false || true;
+console.log(a);
+
+// 다 섞여 있을 경우, 논리 -> AND -> OR  순으로 처리된다.
 
