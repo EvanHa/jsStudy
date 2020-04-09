2f293277bb224a3cf21dd551cab2812b2817cd38
myunghwan <0x4861@neotrilabs.com>
2020-04-09 17:57:48 +0900

    [Add] 조건문 if

--- modern/js/index.js
+++ modern/js/index.js
@@ -1,7 +1,7 @@
-// 비교연산자
-const a = 1;
-const b = 2;
-const equals = a === b; // 3번 입력해야 데이터 타입과 밸류다 같은지 검사
-console.log(equals);
-console.log(a < b);
-console.log(a > b);
+// 조건문
+const a=1;
+if (a+1===2) {
+    const a = 2;
+    console.log('in > a ?= ' + a);
+} 
+console.log('out > a ?= ' + a);
