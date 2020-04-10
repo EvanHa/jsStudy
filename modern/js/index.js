5766786c01d706aeda1b3565e84e394db6ea42aa
myunghwan <0x4861@neotrilabs.com>
2020-04-10 14:12:00 +0900

    [Add] 배열내장함수 filter

--- modern/js/index.js
+++ modern/js/index.js
@@ -1,40 +1,28 @@
 // 배열 내장함수
-const array=[1,2,3,4,5,6,7,8];
 
-const squared=[];
-for (let i=0; i < array.length; i++) {
-    squared.push(array[i] * array[i]);
-}
-console.log(squared);
-
-const squared2=[];
-array.forEach((n)=>{
-    squared2.push(n*n);
-});
-console.log(squared2);
-
-// map 사용
-const square = n=>n*n;
-const squared3 = array.map(square);
-console.log(squared3);
-
-const squared4 = array.map(n=>n*n);
-console.log(squared4);
-
-const items = [
+// filter
+const todos = [
     {
         id:1,
-        text: 'hello'
+        text: 'javascript',
+        done: true,
     },
     {
-        id:2,
-        text: 'bye'
+        id: 2,
+        text: 'function',
+        done: true,
+    },
+    {
+        id: 3,
+        text: 'object',
+        done: true,
+    },
+    {
+        id: 4,
+        text: 'array',
+        done: false
     }
 ];
 
-const texts = items.map(item => item.text);
-console.log(texts);
-
-const superheroes = ['아이언맨', '캡틴아메리카', '토르', '닥터스트레인지'];
-const index = superheroes.indexOf('토르');
-console.log(index);
+const tasksNotDone = todos.filter(todo => todo.done===false);
+console.log(tasksNotDone);
