c7644feb87d15bd86614cca8a3b743a92fe98677
myunghwan <0x4861@neotrilabs.com>
2020-04-10 17:56:26 +0900

    [Add] function parameter

--- modern/js/index.js
+++ modern/js/index.js
@@ -1,42 +1,10 @@
-// 단축 평가 논리 계산법
-const dog = {
-    name: '멍멍이',
-};
-
-function getName(animal) {
-    // if (animal) {
-    //     return animal.name;
-    // } else {
-    //     return undefined;
-    // }
-    // --> 위의 내용은 아래와 같이 바꿀수 있음
-    return animal && animal.name;
+// function parameter
+function calculateCircleArea(r=1) {
+    return Math.PI * r * r;
 }
 
-const name = getName();
-console.log(name);
+const area=calculateCircleArea();
+console.log(area);
 
-
-// 앞에 falsy 가 오면, 뒤에 볼것도 없이 앞에것이 출력
-// 앞에 truthy가 오면, 뒤에 값을 체크하여 결과 출력
-console.log(true && 'hello');
-console.log(false && 'hello');
-console.log('hello' && 'bye');
-console.log(null && 'hello');
-
-const namelessDog = {
-    name: '',
-};
-
-function getName(animal) {
-    const name = animal && animal.name;
-    // if (!name) {
-    //     return '이름없는 동물입니다.'
-    // } else {
-    //     return name;
-    // }
-    return (name || '이름없는 동물입니다.');
-}
-
-const name1 = getName(namelessDog);
-console.log(name1);
+const calculateCircleArea1 = (r=1) => { return Math.PI * r * r; };
+console.log(calculateCircleArea1());
