9a26a8e44fd9ed8fb0ee7c4e4994a226d5db0789
myunghwan <0x4861@neotrilabs.com>
2020-04-10 14:37:43 +0900

    [Add] 배열 내장함수 reduce

--- modern/js/index.js
+++ modern/js/index.js
@@ -1,14 +1,35 @@
 // 배열 내장함수
 
-// concat (기존 배열을 건드리지 않음)
-const arr1=[1,2,3];
-const arr2=[4,5,6];
-const concated=arr1.concat(arr2);
-console.log(concated);
-const spread=[...arr1, ...arr2];
-console.log(spread);
+// reduce (잘사용하면 매우 유용한 함수)
+const numbers = [1,2,3,4,5];
 
-// Join
-const array = [1,2,3,4,5];
-console.log(array.join());
-console.log(array.join(' '));
+let sum = 0;
+numbers.forEach(n=>{
+    sum += n;
+});
+console.log(sum);
+
+// accumulator는 누적된 값
+// current는 배열의 원소들 값이 사용됨
+const reduced = numbers.reduce((accumulator, current)=> accumulator+current, 0);
+console.log("sum : " + reduced);
+// reduce로 평균 계산
+const reduced2 = numbers.reduce((accumulator, current, index, array) => {
+    if (index == array.length - 1) {
+        return (accumulator + current) / array.length;
+    }
+    return accumulator + current;
+}, 0);
+console.log("average : "+reduced2);
+
+
+const alphabets = ['a', 'a', 'b', 'b', 'c', 'c', 'c', 'd'];
+const counts = alphabets.reduce((acc, current,)=> {
+    if (acc[current]) {
+        acc[current] += 1;
+    } else {
+        acc[current] = 1;
+    }
+    return acc;
+}, {});
+console.log(counts);
