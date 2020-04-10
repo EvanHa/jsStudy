a357d9dcfc2b312f7fdb4098984f5938acd3a23b
myunghwan <0x4861@neotrilabs.com>
2020-04-10 14:26:35 +0900

    [Add] 배열 내장함수 concat, join

--- modern/js/index.js
+++ modern/js/index.js
@@ -1,23 +1,14 @@
 // 배열 내장함수
 
-// shift (기존의 배열을 건드림, first out)
-const numbers = [10, 20, 30, 40];
-const value = numbers.shift();
-console.log("shift retrun value : "+value);
-console.log(numbers);
+// concat (기존 배열을 건드리지 않음)
+const arr1=[1,2,3];
+const arr2=[4,5,6];
+const concated=arr1.concat(arr2);
+console.log(concated);
+const spread=[...arr1, ...arr2];
+console.log(spread);
 
-// unshift
-const value1 = numbers.unshift(10);
-console.log("unshift return value : " + value1);
-console.log(numbers);
-
-// pop (last out)
-const value2 = numbers.pop();
-console.log("pop return value : " + value2);
-console.log(numbers);
-
-// push
-const value3 = numbers.push(40);
-console.log("push return value : " + value3);
-console.log(numbers);
-
+// Join
+const array = [1,2,3,4,5];
+console.log(array.join());
+console.log(array.join(' '));
