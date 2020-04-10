c023ea8d516300679af855abb559dc7a71ddcac2
myunghwan <0x4861@neotrilabs.com>
2020-04-10 16:49:34 +0900

    [Add] 상속

--- modern/js/index.js
+++ modern/js/index.js
@@ -1,14 +1,4 @@
-// 객체
-// function Animal(type, name, sound) {
-//     this.type = type;
-//     this.name = name;
-//     this.sound = sound;
-//     this.say = function() {
-//         console.log(this.sound);
-//     }
-// };
-
-// say는 공통적으로 사용되니깐, prototype으로 변경
+// 상속
 function Animal(type, name, sound) {
     this.type = type;
     this.name = name;
@@ -19,8 +9,19 @@ Animal.prototype.say = function() {
     console.log(this.sound);
 }
 
-const dog = new Animal('개', '멍멍이', '멍멍');
-const cat = new Animal('고양이', '야옹이', '야옹');
+function Dog(name, sound) {
+    Animal.call(this, '개', name, sound);
+}
+
+function Cat(name, sound) {
+    Animal.call(this, '고양이', name, sound);
+}
+
+Dog.prototype = Animal.prototype;
+Cat.prototype = Animal.prototype;
+
+const dog = new Dog('멍멍이', '멍멍');
+const cat = new Cat('야옹이', '야옹');
 
 dog.say();
 cat.say();
