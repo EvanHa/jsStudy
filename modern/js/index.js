4de9e938ac1a81d8c27b56008987e44de3e51272
myunghwan <0x4861@neotrilabs.com>
2020-04-10 17:03:25 +0900

    [Add] 삼항연산자

--- modern/js/index.js
+++ modern/js/index.js
@@ -1,54 +1,11 @@
-// class
-class Animal {
-    constructor(type, anme, sound) {
-        this.type = type;
-        this.name = name;
-        this.sound = sound;
-    }
-    say() {
-        console.log(this.sound);
-    }
-}
+// 삼항 연산자
+const array = [];
+// let text = '';
+// if (array.length === 0) {
+//     text = '배열이 비어있습니다.';
+// } else {
+//     text = '배열이 비어있지 않습니다.';
+// }
+let text = (array.length === 0) ? '배열이 비어있습니다':'배열이 비어있지 않습니다'
+console.log(text);
 
-class Dog extends Animal {
-    constructor(name, sound) {
-        super('개', name, sound);
-    }
-}
-
-class Cat extends Animal {
-    constructor(name, sound) {
-        super('고양이', name, sound);
-    }
-}
-
-const dog = new Dog('멍멍이', '멍멍');
-const cat = new Cat('야옹이', '야옹');
-
-dog.say();
-cat.say();
-
-
-class Food {
-    constructor(name) {
-        this.name = name;
-        this.brands = [];
-    }
-    addBrand(brand) {
-        this.brands.push(brand);
-    }
-    print() {
-        console.log(`${this.name} 을 파는 임식점들 : `);
-        console.log(this.brands.join(', '));
-    }
-}
-
-const pizza = new Food('피자');
-pizza.addBrand('피자헛');
-pizza.addBrand('도미노 피자');
-
-const chicken = new Food('치킨');
-chicken.addBrand('굽네치킨');
-
-pizza.print();
-chicken.print();
