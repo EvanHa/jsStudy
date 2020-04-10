f03c38e4ee12ca6780e92763781b555b0bd3c569
myunghwan <0x4861@neotrilabs.com>
2020-04-10 16:59:11 +0900

    [Add] class

--- modern/js/index.js
+++ modern/js/index.js
@@ -1,27 +1,54 @@
-// 상속
-function Animal(type, name, sound) {
-    this.type = type;
-    this.name = name;
-    this.sound = sound;
-};
-
-Animal.prototype.say = function() {
-    console.log(this.sound);
+// class
+class Animal {
+    constructor(type, anme, sound) {
+        this.type = type;
+        this.name = name;
+        this.sound = sound;
+    }
+    say() {
+        console.log(this.sound);
+    }
 }
 
-function Dog(name, sound) {
-    Animal.call(this, '개', name, sound);
+class Dog extends Animal {
+    constructor(name, sound) {
+        super('개', name, sound);
+    }
 }
 
-function Cat(name, sound) {
-    Animal.call(this, '고양이', name, sound);
+class Cat extends Animal {
+    constructor(name, sound) {
+        super('고양이', name, sound);
+    }
 }
-
-Dog.prototype = Animal.prototype;
-Cat.prototype = Animal.prototype;
 
 const dog = new Dog('멍멍이', '멍멍');
 const cat = new Cat('야옹이', '야옹');
 
 dog.say();
 cat.say();
+
+
+class Food {
+    constructor(name) {
+        this.name = name;
+        this.brands = [];
+    }
+    addBrand(brand) {
+        this.brands.push(brand);
+    }
+    print() {
+        console.log(`${this.name} 을 파는 임식점들 : `);
+        console.log(this.brands.join(', '));
+    }
+}
+
+const pizza = new Food('피자');
+pizza.addBrand('피자헛');
+pizza.addBrand('도미노 피자');
+
+const chicken = new Food('치킨');
+chicken.addBrand('굽네치킨');
+
+pizza.print();
+chicken.print();
