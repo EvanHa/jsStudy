d9abb2d615d017b9f31a8ed45dc6fe5cebf23f8a
myunghwan <0x4861@neotrilabs.com>
2020-04-10 20:37:15 +0900

    [Add] hoisting

--- modern/js/index.js
+++ modern/js/index.js
@@ -1,55 +1,15 @@
-// scope
-// 1. global 
-// 2. function (function 내부)
-// 3. block (if, for, switch )
-
-// const value = 'hello';
-
-// function myFunction() {
-//     console.log('myFunction: ');
-//     console.log(value);
-// }
-
-// // 함수형 스코프
-// function otherFunction() {
-//     console.log('otherFunction: ');
-//     const value = 'bye!';
-//     console.log(value);
-// }
-
-// myFunction();
-// otherFunction();
-
-// console.log('global scope: ');
-// console.log(value);
-
-// function myFunction() {
-//     const value = 'bye!';
-//     const anotherValue = 'world';
-//     function functionInside() {
-//         console.log('functionInside: ');
-//         console.log(value);
-//         console.log(anotherValue);
-//     }
-//     functionInside();
-// }
-
-// myFunction();
-// console.log('global scope:');
-// console.log(value);
-// console.log(anotherValue);
-
-function myFunction() {
-    const value = 'bye!';
-    if (true) {
-        const value = 'world';
-        console.log('block scope: ');
-        console.log(value);
-    }
-    console.log('function scope');
-    console.log(value);
-}
+// hoisting
 
 myFunction();
-console.log('global scope:');
-console.log(value);
+
+function myFunction() {
+    console.log('hello world');
+}
+
+console.log(temp)
+var temp = 2;
+// ==> 위의 코드는 아래 코드로 처리됨.
+// var temp
+// console.log(temp)
+// temp = 2
+
