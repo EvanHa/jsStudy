d19b854e33b9f1d3fdc5e68433a9cf74f0d1a0b9
myunghwan <0x4861@neotrilabs.com>
2020-04-10 17:11:45 +0900

    [Add] Truthy, Falsy

--- modern/js/index.js
+++ modern/js/index.js
@@ -1,11 +1,25 @@
-// 삼항 연산자
-const array = [];
-// let text = '';
-// if (array.length === 0) {
-//     text = '배열이 비어있습니다.';
-// } else {
-//     text = '배열이 비어있지 않습니다.';
-// }
-let text = (array.length === 0) ? '배열이 비어있습니다':'배열이 비어있지 않습니다'
-console.log(text);
+// Truthy (true 같은거) 
+function print(person) {
+    //if ((person === undefined) || (person === null)) { 
+    if (!person) {
+        return ;
+    }
+    console.log(person.name);
+}
 
+const person= {
+    name: 'John',
+};
+
+print();
+
+
+// Falsy (false 같은거)
+// javascript에서는 undefined, null을 false로 간주한다. 그래서 아래 구문은 true가 찍힘..
+// 아래 내용을 제외하고는 Truthy한 값으로 취급
+console.log(!undefined);
+console.log(!null);
+console.log(!0);
+console.log(!'');
+console.log(!NaN);
+console.log(!false);
