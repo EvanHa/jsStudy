20a3edda29b026107fe489df3c4e5e6aabdb935d
myunghwan <0x4861@neotrilabs.com>
2020-04-10 23:58:22 +0900

    [Add] Promise

--- modern/js/index.js
+++ modern/js/index.js
@@ -1,21 +1,50 @@
-// 비동기 처리
+// Promise
+// 비동기작업을 더 편하게 처리할 수 있도록 지원해주는 ES6기능
 
-function work(callback) {
-    setTimeout(()=> {
-        const start = Date.now();
-        for (let i=0; i < 1000000000; i++) {
+// const myPromise = new Promise((resolve, reject)=>{
+//     setTimeout(()=>{
+//         //resolve('result');
+//         reject(new Error());
+//     },1000);
+// });
 
-        }
-        const end = Date.now();
-        console.log(end-start , 'ms');
-        callback(end-start);
-    }, 0);
-    
+// myPromise.then(result=>{
+//     console.log(result);
+// }).catch(e=>{
+//     console.error(e);
+// });
+
+
+// function increaseAndPrint(n, callback) {
+//     setTimeout(()=>{
+//         const increased = n +1;
+//         console.log(increased);
+//         if (callback) {
+//             callback(increased);
+//         }
+//     }, 1000);
+// }
+
+function increaseAndPrint(n) {
+    return new Promise((resolve, reject)=>{
+        setTimeout(()=>{
+            const value = n+1;
+            if (value === 5) {
+                const error = new Error();
+                error.name = 'ValueIsFiveError';
+                reject(error);
+                return;
+            }
+            console.log(value);
+            resolve(value);
+        }, 1000);
+    });
 }
 
-console.log('작업 시작');
-work((ms)=>{
-    console.log('작업이 끝났어요!');
-    console.log(ms + 'ms 걸렸다고 해요');
-});
-console.log('다음 작업');
+increaseAndPrint(2).then(n=>{
+    return increaseAndPrint(n);
+}).then(n=>{
+    return increaseAndPrint(n);
+}).catch(e=>{
+    console.error(e);
+})
