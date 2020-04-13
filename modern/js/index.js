5b460081fa9164f3edf78d1265f45f44954c9e9a
myunghwan <0x4861@neotrilabs.com>
2020-04-13 12:30:01 +0900

    [Add] promise all, race

--- modern/js/index.js
+++ modern/js/index.js
@@ -1,31 +1,48 @@
-// async await
+// promise.all, promise.race
+// promise all 은 배열의 함수들이 모두 끝나면 결과를 리턴한다.
+// promise race는 배열의 함수들 중 가장 빨리끝나는 함수의 결과를 리턴한다.
 function sleep(ms) {
     return new Promise(resolve => setTimeout(resolve, ms));
 }
 
+const getDog = async () => {
+    await sleep(1000);
+    return '멍멍이';
+};
+
+const getRabbit = async () => {
+    await sleep(500);
+    return '토끼';
+};
+
+const getTurttle = async () => {
+    await sleep(3000);
+    return '거북이';
+};
+
 async function process() {
-    console.log('안녕하세요');
-    await sleep(1000);
-    console.log('반갑습니다.');
-    return true;
-}
-
-process().then(value => {
-    console.log(value);
-});
-
-async function makeError() {
-    await sleep(1000);
-    const error = new Error();
-    throw error; // error를 생성할 때는 throw를 사용
-}
-
-async function process2() {
     try {
-        await makeError();
+        const result = await Promise.all([getDog(), getRabbit(), getTurttle()]);
+        console.log(result);
+    } catch (e) {
+        console.error(e);
+    }
+
+    try {
+        const [dog, rabbit, turttle] = await Promise.all([getDog(), getRabbit(), getTurttle()]);
+        console.log(dog);
+        console.log(rabbit);
+        console.log(turttle);
+    } catch (e) {
+        console.error(e);
+    }
+
+    try {
+        const first = await Promise.race([getDog(), getRabbit(), getTurttle()]);
+        console.log(first);
     } catch (e) {
         console.error(e);
     }
 }
 
-process2();
+process();
