ddde8a044c793fcb5b98da764c969fb0eb9f3e74
myunghwan <0x4861@neotrilabs.com>
2020-04-13 11:05:29 +0900

    [Add] async, await

--- modern/js/index.js
+++ modern/js/index.js
@@ -1,50 +1,31 @@
-// Promise
-// 비동기작업을 더 편하게 처리할 수 있도록 지원해주는 ES6기능
-
-// const myPromise = new Promise((resolve, reject)=>{
-//     setTimeout(()=>{
-//         //resolve('result');
-//         reject(new Error());
-//     },1000);
-// });
-
-// myPromise.then(result=>{
-//     console.log(result);
-// }).catch(e=>{
-//     console.error(e);
-// });
-
-
-// function increaseAndPrint(n, callback) {
-//     setTimeout(()=>{
-//         const increased = n +1;
-//         console.log(increased);
-//         if (callback) {
-//             callback(increased);
-//         }
-//     }, 1000);
-// }
-
-function increaseAndPrint(n) {
-    return new Promise((resolve, reject)=>{
-        setTimeout(()=>{
-            const value = n+1;
-            if (value === 5) {
-                const error = new Error();
-                error.name = 'ValueIsFiveError';
-                reject(error);
-                return;
-            }
-            console.log(value);
-            resolve(value);
-        }, 1000);
-    });
+// async await
+function sleep(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
 }
 
-increaseAndPrint(2).then(n=>{
-    return increaseAndPrint(n);
-}).then(n=>{
-    return increaseAndPrint(n);
-}).catch(e=>{
-    console.error(e);
-})
+async function process() {
+    console.log('안녕하세요');
+    await sleep(1000);
+    console.log('반갑습니다.');
+    return true;
+}
+
+process().then(value => {
+    console.log(value);
+});
+
+async function makeError() {
+    await sleep(1000);
+    const error = new Error();
+    throw error; // error를 생성할 때는 throw를 사용
+}
+
+async function process2() {
+    try {
+        await makeError();
+    } catch (e) {
+        console.error(e);
+    }
+}
+
+process2();
