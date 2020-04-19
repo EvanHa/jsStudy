c37a53ec32971604d622309bd8b07e69e6f61e41
Evan Ha <0x4861@gmail.com>
2020-04-20 00:16:07 +0900

    [Add] weakmap

--- es6/index.js
+++ es6/index.js
@@ -1,21 +1,56 @@
-// weakset
-// 참조를 가지고 있는 개체만 저장이 가능.
-// 객체형태를 중복없이 저장하려고 할 때 유용하다.
+// map & weakmap
+// Array -> set, weakset
+// Object -> map, weakmap
+// 어레이를 개선할려고 한것이 set, weakset이고
+// 객체를 개선하려고 한 것이 map, weakmap 이다
 
-let arr = [1,2,3,4];
-let arr2 = [5,6,7,8];
-let obj = {arr, arr2};
-let ws = new WeakSet();
+// map은 key/value 로 저장
 
-ws.add(arr);
-//ws.add(111);
-//ws.add("111");
-//ws.add(null);
-ws.add(function(){});
-ws.add(arr2);
-ws.add(obj);
+let wm = new WeakMap();
+let myFun = function() {};
+// 이 함수가 얼마나 실행됐지?를 알려고 할 때, 호출된 숫자 count하는 함수를 만들어 보자
 
-arr = null; // 참조를 읽기 때문에, ws.has(arr)을 해보면 false가 출력됨
+wm.set(myFun, 0);
+console.log(wm);
 
-console.log(ws);
-console.log(ws.has(arr), ws.has(arr2));
+let count=0;
+for(let i=0; i<10; i++) {
+    count = wm.get(myFun); // get value
+    count++;
+    wm.set(myFun, count);
+}
+console.log(wm.get(myFun));
+
+myFun = null;
+console.log(wm.get(myFun));
+console.log(wm.has(myFun));
+
+
+// WeakMap 활용
+
+function Area(height, width) {
+    this.height = height;
+    this.width = width;
+}
+
+Area.prototype.getArea = function () {
+    return this. height * this.width;
+}
+
+let myarea = new Area(10,20);
+console.log(myarea.getArea());
+console.log(myarea.height);
+
+
+const wmap = new WeakMap();
+
+function Area2(height, width) {
+    wm.set(this, {height, width});
+}
+Area2.prototype.getArea = function() {
+    const {height, width} = wm.get(this);
+    return height * width;
+}
+
+let myarea2 = new Area2(10, 20);
+console.log(myarea2.getArea());
