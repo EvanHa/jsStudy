3cd696f4f3203538faa67a461223a83fcf0d34b9
Evan Ha <0x4861@gmail.com>
2020-04-20 00:30:04 +0900

    [Add] Destructuring and set example

--- es6/index.js
+++ es6/index.js
@@ -1,56 +1,31 @@
-// map & weakmap
-// Array -> set, weakset
-// Object -> map, weakmap
-// 어레이를 개선할려고 한것이 set, weakset이고
-// 객체를 개선하려고 한 것이 map, weakmap 이다
+// Destructuring and Set (create lotto number)
 
-// map은 key/value 로 저장
-
-let wm = new WeakMap();
-let myFun = function() {};
-// 이 함수가 얼마나 실행됐지?를 알려고 할 때, 호출된 숫자 count하는 함수를 만들어 보자
-
-wm.set(myFun, 0);
-console.log(wm);
-
-let count=0;
-for(let i=0; i<10; i++) {
-    count = wm.get(myFun); // get value
-    count++;
-    wm.set(myFun, count);
-}
-console.log(wm.get(myFun));
-
-myFun = null;
-console.log(wm.get(myFun));
-console.log(wm.has(myFun));
-
-
-// WeakMap 활용
-
-function Area(height, width) {
-    this.height = height;
-    this.width = width;
+const SETTING = {
+    name : "LUCKY LOTTO",
+    count : 6,
+    maxNumber : 45    
 }
 
-Area.prototype.getArea = function () {
-    return this. height * this.width;
+let result_set = new Set();
+
+function getRandomNumber(maxNumber) {
+    // 랜덤한 유일한 숫자값을 추출
+    // TODO:
+    let prev_size = result_set.size;
+    let curr_size = prev_size;
+    while(curr_size === prev_size) {
+        let new_number = (Math.floor(Math.random() * maxNumber)) + 1; // 1 ~ maxNumber
+        result_set.add(new_number);
+        curr_size = result_set.size;
+    }
 }
 
-let myarea = new Area(10,20);
-console.log(myarea.getArea());
-console.log(myarea.height);
-
-
-const wmap = new WeakMap();
-
-function Area2(height, width) {
-    wm.set(this, {height, width});
-}
-Area2.prototype.getArea = function() {
-    const {height, width} = wm.get(this);
-    return height * width;
+let {count:cnt, maxNumber:max} = SETTING;
+for (let i=0; i<cnt; i++) {
+    getRandomNumber(max);
 }
 
-let myarea2 = new Area2(10, 20);
-console.log(myarea2.getArea());
+console.log(result_set);
+
+
+
