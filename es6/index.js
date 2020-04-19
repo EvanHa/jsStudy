ca033bbd64c841e8de8c826bcee217e77aaac3da
Evan Ha <0x4861@gmail.com>
2020-04-19 10:53:54 +0900

    [Mod] 오타 수정

--- es6/index.js
+++ es6/index.js
@@ -9,7 +9,7 @@ Array.prototype.getIndex = function() {};
 for(let idx in data) {
     console.log(data[idx]);
 };
-// for in을 썼을때는 상위(prototype)의 것도 순해해서 출력해버린다.
+// for in을 썼을때는 상위(prototype)의 것도 순회해서 출력해버린다.
 
 for(let value of data) {
     console.log(value);
