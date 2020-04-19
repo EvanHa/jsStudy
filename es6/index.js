23e9dc433178b87a2e3550e4b3c96b5b5dcac589
myunghwan <0x4861@neotrilabs.com>
2020-04-19 14:22:15 +0900

    [Add] Set

--- es6/index.js
+++ es6/index.js
@@ -1,34 +1,25 @@
-// Destructuring 활용 event 객체 전달
+// set으로 유니크한 배열만들기
 
-let news = [
-    {
-        "title":"sbs",
-        "imgurl":"blabla",
-        "newslist" : [
-            "1",
-            "2",
-            "3"
-        ]
-    },
-    {
-        "title": "mbc",
-        "imgurl" : "dddd",
-        "newslist" : [
-            "5",
-            "6",
-            "7"
-        ]
-    }
-];
+let mySet = new Set();
+console.log(toString.call(mySet));
 
-function getNewsList([,{newslist}]) {
-    console.log(newslist);
+// set : 중복없이 유일한 값을 저장하려고 할때, 이미 존재하는지 체크할 때 유용.
+
+mySet.add("crong");
+mySet.add("harry");
+mySet.add("crong"); // 이미 crong이 있으니깐, 이 값은 저장 안된다.
+
+if(mySet.has("crong")) {
+    console.log("true");
 }
 
-getNewsList(news);
+mySet.forEach(function(v) {
+    console.log(v);
+});
+
+mySet.delete("crong");
 
 
-
-document.querySelector("div").addEventListener("click",function({target}) {
-    console.log(target.innerText);
+mySet.forEach(function(v) {
+    console.log(v);
 });
