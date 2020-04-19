6d8fde9f6ed6c22791506de707c911cd20ed1acd
Evan Ha <0x4861@gmail.com>
2020-04-19 11:03:01 +0900

    [add] spread operator

--- es6/index.js
+++ es6/index.js
@@ -1,21 +1,30 @@
-// array for of
+// spread operator, 펼침연산자.
 
-let data = [1,2,undefined,NaN,null,""];
-data.forEach(function(value){
-    console.log(value);
-});
+let pre = ["apple", "orange", 100];
+let newData = [...pre];
 
-Array.prototype.getIndex = function() {};
-for(let idx in data) {
-    console.log(data[idx]);
-};
-// for in을 썼을때는 상위(prototype)의 것도 순회해서 출력해버린다.
+console.log(pre);
+console.log(newData);
+console.log(pre === newData);
 
-for(let value of data) {
-    console.log(value);
+// 참조가 아니라, 새로운 것을 생성해서 갖고 있다. concat 사용한 것 같은 효과
+// 참조가 아니기 때문에 pre newData는 다른 객체인 것.
+
+
+// 응용
+let pre1 = [100, 200, "hello", null];
+let newData1 = [0,1,2,3,...pre,4,];
+console.log(pre1);
+console.log(newData1);
+
+
+
+function sum(a,b,c) {
+    return a+b+c;
 }
-
-let str = "hello world!!!";
-for(let value of str) {
-    console.log(value);
-}
+let pre2 = [100, 200, 300];
+console.log(sum.apply(null, pre2));
+console.log(sum(100,200,300));
+console.log(sum(pre2[0],pre2[1],pre2[2]));
+// -> 위와 같은 일을 하는 새로운 방법
+console.log(sum(...pre));
