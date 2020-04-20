354f7195ddc85773b75cb92e8f10e34d45134163
myunghwan <0x4861@neotrilabs.com>
2020-04-20 11:00:56 +0900

    [Add] rest parameters

--- es6/index.js
+++ es6/index.js
@@ -1,28 +1,11 @@
-// this context of arrow function
+// rest parameters
 
-const myObj = {
-    runTimeout() {
-        setTimeout(function(){
-            console.log(this === window);
-            this.printData();
-        }.bind(this), 200);
-    },
-    printData() {
-        console.log("hi codesquad!");
-    }
+// 가변 인자 파라마터가 모두 숫자인지 체크 하는 함수
+// 하나라도 숫자가 아닌 경우 false return
+function checkNum(...argArray) {
+    console.log(toString.call(argArray));
+    const result = argArray.every((v)=>typeof v === "number");
+    console.log(result);
 }
 
-const myObj1 = {
-    runTimeout() {
-        setTimeout(()=>{
-            console.log(this === window);
-            this.printData();
-        }, 200);
-    },
-    printData() {
-        console.log("hi codesquad!");
-    }
-}
-
-myObj.runTimeout();
-myObj1.runTimeout();
+const result = checkNum(10,2,3,4,5,"55");
