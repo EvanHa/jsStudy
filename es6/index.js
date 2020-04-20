4851817e4e254ac53ecf38bd3ce490e00c432729
myunghwan <0x4861@neotrilabs.com>
2020-04-20 11:49:09 +0900

    [Add] setPrototypeOf

--- es6/index.js
+++ es6/index.js
@@ -1,12 +1,21 @@
-// Object assign 메서드
-const prevObj = {
+// setPrototypeOf
+
+const healthObj = {
+    showHealth : function() {
+        console.log("오늘 운동시간 : " + this.healthTime);
+    },
+    setHealth : function (newTime) {
+        this.healthTime = newTime;
+    }
+}
+
+const myHealth = {
     name : "crong",
     lastTime : "11:20"
 }
-const myHealth = Object.assign({},prevObj,{
-    lastTime : "12:30",
-    name : "honux",
-    age : 99
-});
-console.log("myHealth is ", myHealth);
+
+const newObj = Object.setPrototypeOf(myHealth, healthObj); // healthObj의 함수들이 myHealth의 prototype으로 들어감.
+console.log("myhealth is", myHealth);
+console.log("healthObj is", healthObj);
+console.log(newObj);
 
