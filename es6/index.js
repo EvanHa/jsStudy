7c01e99f6196c7281c7f35b6ef1633b66e9b1d51
myunghwan <0x4861@neotrilabs.com>
2020-04-20 11:42:36 +0900

    [Add] Object assign Immutable

--- es6/index.js
+++ es6/index.js
@@ -1,19 +1,12 @@
 // Object assign 메서드
-const healthObj = {
-    showHealth : function() {
-        console.log("오늘 운동시간 " + this.healthTime);
-    }
-}
-
-const myHealth = Object.create(healthObj);
-myHealth.healthTime = "11:20";
-myHealth.name = "crong";
-
-console.log(myHealth);
-
-
-const myHealth1 = Object.assign(Object.create(healthObj),{
+const prevObj = {
     name : "crong",
     lastTime : "11:20"
+}
+const myHealth = Object.assign({},prevObj,{
+    lastTime : "12:30",
+    name : "honux",
+    age : 99
 });
 console.log("myHealth is ", myHealth);
+
