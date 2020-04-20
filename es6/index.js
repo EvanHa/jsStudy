e1f4d931f729e3292983509b87bf11a84ab114f0
myunghwan <0x4861@neotrilabs.com>
2020-04-20 12:34:31 +0900

    [Add] proxy

--- es6/index.js
+++ es6/index.js
@@ -1,24 +1,6 @@
-// setPrototypeOf
+// proxy.
 
-// Parent
-const healthObj = {
-    showHealth : function() {
-        console.log("오늘 운동시간 : " + this.healthTime);
-    },
-    setHealth : function (newTime) {
-        this.healthTime = newTime;
-    }
-}
+const myObj = { name: 'crong' };
+const proxy = new Proxy( myObj, {});
 
-// Child obj
-const heathChildObj = {
-    getAge : function () {
-        return this.age; 
-    }
-}
-
-Object.setPrototypeOf(heathChildObj, healthObj);
-const childOjb = Object.setPrototypeOf({
-    age : 22
-}, heathChildObj);
-console.log("childobj is ", childOjb);
+console.log(proxy.name);
